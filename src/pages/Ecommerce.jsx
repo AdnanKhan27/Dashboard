e4309a5ae60f539127e57adbe5fa35efc15c268b
Adnan Khan <adnan.khan2019@vitbhopal.ac.in>
2022-10-17 13:05:08 +0530

    New: added revenue updates added charts - SparkLine chart - Stacked charts for budget & expenses

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -131,6 +131,20 @@ const Ecommerce = () => {
                   color="blue"
                 />
               </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+            <div>
+                <Stacked 
+                  width="320px" 
+                  height="360px"
+                />
             </div>
           </div>
         </div>
