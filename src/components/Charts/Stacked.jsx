f5289016cf996bc459e440eb716d05019ad643b9
Adnan Khan <adnan.khan2019@vitbhopal.ac.in>
2022-10-20 12:54:18 +0530

    - Fixed the legend color not changing with changing theme mode - Used the textStyle api inside the legendSettings prop for the charts component     - added the color to textStyle dynamically using the currentMode stateContext from contextProvider

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+  Highlight
+} from "@syncfusion/ej2-react-charts";
 
-import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
+
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Stacked = ({ width, height }) => {
+  const { currentMode } = useStateContext(); //getting the current active mode (dark/light) from contexts
+  
   return (
     <ChartComponent
       width={width}
@@ -11,16 +29,21 @@ const Stacked = ({ width, height }) => {
       id="charts"
       primaryXAxis={stackedPrimaryXAxis}
       primaryYAxis={stackedPrimaryYAxis}
-      chartArea={{ border: { width: 0 }}}
-      tooltip={{ enable:true }}
-      legendSettings={{ background: 'white'}}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      /* 👇changing the legend text color acc. to theme mode(dark/light) */
+      legendSettings={{ textStyle: {color: `${currentMode === "Dark" ? "#fff" : "#33373E"}` } }}
+      /* 👇changing the background color of whole chart acc. to theme mode(dark/light) */
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
     >
-      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip, Highlight]} />
       <SeriesCollectionDirective>
-        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+        {stackedCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
       </SeriesCollectionDirective>
     </ChartComponent>
-  )
-}
+  );
+};
 
-export default Stacked
+export default Stacked;
