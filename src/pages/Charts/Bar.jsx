469620891e7c46acbe169adccde9462907025ad0
Adnan Khan <adnan.khan2019@vitbhopal.ac.in>
2022-10-31 12:03:31 +0530

    Fix: Title in Bar graph page

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -25,7 +25,7 @@ const Bar = () => {
 
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <Header category="Bar" title="Sales Rate etPercentage" />
+      <Header category="Bar" title="Sales Rate Percentage" />
       <div className="w-full ">
         <ChartComponent
           id="bar-chart"
