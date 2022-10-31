fe6593822f33cba09ce32cc5556abf03498a9b03
Adnan Khan <adnan.khan2019@vitbhopal.ac.in>
2022-10-31 13:05:40 +0530

    New: Adding the github pages config - added scripts to package.json     - "predeploy" : "npm run build",     - "deploy" : "gh-pages -d build"
    Also: adding the Pie chart component

--- src/components/Charts/Pie.jsx
+++ src/components/Charts/Pie.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  PieSeries,
+  Inject,
+  AccumulationLegend,
+  AccumulationDataLabel,
+  AccumulationTooltip
+} from "@syncfusion/ej2-react-charts";
+
+import { Header } from "../../components";
+import { pieChartData } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Pie = () => {
-  return (
-    <div>Pie</div>
-  )
-}
+  const { currentMode } = useStateContext();
 
-export default Pie
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Pie" title="Sales Rate Percentage" />
+      <div className="w-full ">
+        <AccumulationChartComponent 
+        id="charts"
+        height="420px"
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373E" : "#fff"}
+        legendSettings={{
+          textStyle: {
+            color: `${currentMode === "Dark" ? "#fff" : "#33373E"}`,
+          },
+        }}
+        >
+          <Inject services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]} />
+          <AccumulationSeriesCollectionDirective>
+            <AccumulationSeriesDirective 
+              
+            />
+          </AccumulationSeriesCollectionDirective>
+        </AccumulationChartComponent>
+
+      </div>
+    </div>
+  );
+};
+
+export default Pie;
--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Pie = () => {
   return (
@@ -6,4 +6,4 @@ const Pie = () => {
   )
 }
 
-export default Pie
+export default Pie;
